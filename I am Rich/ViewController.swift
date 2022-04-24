cc37552c1615e8bebfe18c1dd4281dde9e548b38
leohb07 <leohb1501@gmail.com>
2022-04-24 16:48:21 -0300

    Projeto finalizado

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Leonardo Henrique Barrocal on 12/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+}
